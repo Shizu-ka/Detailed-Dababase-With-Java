c65cba1056a29f9ffa7045948a288cc0a06d8c10
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2022-11-07 15:14:47 +0700

    Update DatabaseGUI.java

--- DatabaseGUI/src/databasegui/DatabaseGUI.java
+++ DatabaseGUI/src/databasegui/DatabaseGUI.java
@@ -10,9 +10,7 @@ package databasegui;
  */
 public class DatabaseGUI {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("hello");
     }
