11005d5ffa8fdd057fa20ac44323b4a05ddfaf12
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2022-11-09 16:44:04 +0700

    add connection sql

--- DatabaseGUI/src/databasegui/DatabaseGUI.java
+++ DatabaseGUI/src/databasegui/DatabaseGUI.java
@@ -4,6 +4,9 @@
  */
 package databasegui;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author Lenovo
@@ -11,8 +14,22 @@ package databasegui;
 public class DatabaseGUI {
 
     
-    public static void main(String[] args) {
-        System.out.println("hello");
+    public static void main(String[] args)  throws ClassNotFoundException, SQLException {
+         String hostname = "localhost";
+            String sqlInstanceName = "LAPTOP-43HAT1M2"; //computer name 
+            String sqlDatabase = "University";  //sql server database name
+            String sqlUser = "123";
+            String sqlPassword = "123"; //passwrod sa account
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            //jdbc:sqlserver://localhost:1433;instance=COMPUTERBERRY;databaseName=Database;
+            String connectURL = "jdbc:sqlserver://" + hostname + ":1433" 
+                    + ";instance=" + sqlInstanceName + ";databaseName=" + sqlDatabase + ";encrypt=true;trustServerCertificate=true";
+
+            Connection conn = DriverManager.getConnection(connectURL, sqlUser, sqlPassword);
+            System.out.println("Connect to database successful!!"); 
+            
+            
     }
-    
 }
