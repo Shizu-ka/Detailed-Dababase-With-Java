05ffdd602fee85cc8d7a51d1860d9a0bfa5e4c15
wahyuvlntn <91378386+wahyuvlntn@users.noreply.github.com>
2022-11-09 08:07:41 +0700

    testing wahyu

--- DatabaseGUI/src/databasegui/DatabaseGUI.java
+++ DatabaseGUI/src/databasegui/DatabaseGUI.java
@@ -14,6 +14,7 @@ public class DatabaseGUI {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("testing wahyu");
+        System.out.println("another testing");
     }
     
 }
