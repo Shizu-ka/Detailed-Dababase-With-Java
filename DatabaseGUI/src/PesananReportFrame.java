da6378410811c0f9d2a9702b6f1fc3d6cf30d13f
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2022-12-07 21:54:12 +0700

    Update PesananReportFrame.java

--- DatabaseGUI/src/PesananReportFrame.java
+++ DatabaseGUI/src/PesananReportFrame.java
@@ -1,18 +1,17 @@
 
 import net.sf.jasperreports.engine.*;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.view.JasperViewer;
-
 import java.io.*;
-import java.net.URL;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -21,6 +20,58 @@ import java.util.logging.Logger;
 public class PesananReportFrame extends javax.swing.JFrame {
 
     private static String destFileName = "report.pdf";
+    private static Connection conn;
+    private static String primaryKeyNow = "";
+    private static String kodePembayaran = "";
+
+
+    private static void connect() {
+        String hostname = "localhost";
+        String sqlInstanceName = "LAPTOP-43HAT1M2"; //computer name 
+        String sqlDatabase = "basdat";  //sql server database name
+        String sqlUser = "123";
+        String sqlPassword = "123"; //passwrod sa account
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //jdbc:sqlserver://localhost:1433;instance=COMPUTERBERRY;databaseName=Database;
+        String connectURL = "jdbc:sqlserver://" + hostname + ":1433"
+                + ";instance=" + sqlInstanceName + ";databaseName=" + sqlDatabase + ";encrypt=true;trustServerCertificate=true";
+
+        try {
+            PesananReportFrame.conn = DriverManager.getConnection(connectURL, sqlUser, sqlPassword);
+
+        } catch (SQLException ex) {
+            Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("Connect to database successful!!");
+    }
+
+    void tampilkanData() throws ClassNotFoundException, SQLException {
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("ID Pesanan");
+        model.addColumn("Username Pembeli");
+        model.addColumn("Kode Pembayaran");
+        model.addColumn("No Kasir");
+        model.addColumn("Total");
+
+
+        connect();
+        int no = 1;
+        Statement st = conn.createStatement();
+        String cth = "select * from Pesanan";
+        ResultSet resultSet = st.executeQuery(cth);
+        while (resultSet.next()) {
+            model.addRow(new Object[]{resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getInt(5)});
+        }
+        tblPesanan.setModel(model);
+        tblPesanan.getColumnModel().getColumn(0).setPreferredWidth(50);
+        tblPesanan.getColumnModel().getColumn(1).setPreferredWidth(100);
+         tblPesanan.getColumnModel().getColumn(2).setPreferredWidth(100);
+    }
 
     public PesananReportFrame() {
         initComponents();
@@ -31,6 +82,11 @@ public class PesananReportFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         btnPrint = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblPesanan = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        tfKodePembayaran = new javax.swing.JTextField();
+        btnKembali = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -41,21 +97,70 @@ public class PesananReportFrame extends javax.swing.JFrame {
             }
         });
 
+        tblPesanan.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        tblPesanan.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                tblPesananMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(tblPesanan);
+
+        jLabel1.setText("Kode Pembayaran");
+
+        tfKodePembayaran.setEditable(false);
+
+        btnKembali.setText("Kembali");
+        btnKembali.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnKembaliActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
-                .addComponent(btnPrint, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(262, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(tfKodePembayaran, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnPrint, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(btnKembali))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(141, Short.MAX_VALUE)
-                .addComponent(btnPrint)
-                .addGap(136, 136, 136))
+                .addContainerGap(19, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnPrint)
+                    .addComponent(tfKodePembayaran, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 348, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnKembali)
+                .addGap(12, 12, 12))
         );
 
         pack();
@@ -76,7 +181,7 @@ public class PesananReportFrame extends javax.swing.JFrame {
             Connection con = DriverManager.getConnection(connectionURL, sqlUser, sqlPassword);
             JasperReport Bill = getJasperReport();
             Map<String, Object> parameters = new HashMap<>();
-            parameters.put("kodePembayaran",10027);
+            parameters.put("kodePembayaran", kodePembayaran);
             JasperPrint jasperPrint = JasperFillManager.fillReport(Bill, parameters, con);
             JasperViewer.viewReport(jasperPrint, false);
             JasperExportManager.exportReportToPdfFile(jasperPrint, destFileName);
@@ -90,10 +195,28 @@ public class PesananReportFrame extends javax.swing.JFrame {
 
     }//GEN-LAST:event_btnPrintActionPerformed
 
+    private void tblPesananMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblPesananMouseClicked
+        int baris = tblPesanan.rowAtPoint(evt.getPoint());
+        String password = tblPesanan.getValueAt(baris, 2).toString();
+        tfKodePembayaran.setText(password);
+        kodePembayaran = password;
+
+    }//GEN-LAST:event_tblPesananMouseClicked
+
+    private void btnKembaliActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnKembaliActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_btnKembaliActionPerformed
+
     public static void main(String args[]) throws FileNotFoundException, JRException {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new PesananReportFrame().setVisible(true);
+                PesananReportFrame frame = new PesananReportFrame();
+                frame.setVisible(true);
+                try {
+                    frame.tampilkanData();
+                } catch (ClassNotFoundException | SQLException ex) {
+                    Logger.getLogger(PesananReportFrame.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
 
         });
@@ -111,6 +234,11 @@ public class PesananReportFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnKembali;
     private javax.swing.JButton btnPrint;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblPesanan;
+    private javax.swing.JTextField tfKodePembayaran;
     // End of variables declaration//GEN-END:variables
 }
