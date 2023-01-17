e1468ab6ade30a91ec92cfadbb37d7211628d6da
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-01-17 10:54:02 +0700

    Create kueriInsertSisanya.sql

---
+++ query/kueriInsertSisanya.sql
@@ -0,0 +1,45 @@
+select * from Cabang
+insert into Cabang values('LondonNoCap', 'Sachtjen', 'Mendota','Prairieview', 722)
+insert into Cabang values('MalangPhd', 'Manitowish', 'Chengui','Prairieview', 23)
+insert into Cabang values('JakartaPz', 'Loeprich', 'Mcguire','Stockholm', 566)
+
+
+select * from Kasir
+insert into Kasir values('81293674580', HASHBYTES('SHA2_512', 'sukakamu99'),'Ahmad','Sadi','Sasuke','1999-01-12','sasukekamu55',4555555,'JakartaPz')
+insert into Kasir values('87864880743', HASHBYTES('SHA2_512', 'kudalumping66'),'Kakashi','Kuat','Hidayat','1990-08-25','hidayataki78',4555555,'JakartaPz')
+
+insert into Kasir values('81197246877', HASHBYTES('SHA2_512', 'boy4690'),'Mursid',null,'Azza','1983-12-03','mursid5picis',7555555,'LondonNoCap')
+insert into Kasir values('81357480099', HASHBYTES('SHA2_512', 'ganteng2190'),'Basuki','Teddy','Akatsuki','2000-01-30','akatsuka88',7555555,'LondonNoCap')
+insert into Kasir values('87855443399', HASHBYTES('SHA2_512', '99musuhgtg'),'Giring',null,'Neji','2001-05-12','sahabataku77',7555555,'LondonNoCap')
+
+insert into Kasir values('1703309667', HASHBYTES('SHA2_512', 'bscafe3x982'),'Ancell','Dyane','Spiaggia','1975-09-26','glaverack41',3555555,'MalangPhd')
+insert into Kasir values('8135673041', HASHBYTES('SHA2_512', 'lbirkmyr46481'),'Guy','Logan','Birkmyr','1991-07-27','lbirkmyr46',3555555,'MalangPhd')
+
+
+select * from Pengantar
+insert into Pengantar values ('bowocoewie7','B1654BCE','81237765890',HASHBYTES('SHA2_512', 'safety1990'), 'Bowo', 'Coe', 'Wie',4000000,'JakartaPz')
+insert into Pengantar values ('frenkdejong10','BD6298SJK','85643249087',HASHBYTES('SHA2_512', 'kohler2020'), 'Frank', 'De', 'Jong',4000000,'JakartaPz')
+
+insert into Pengantar values ('robegojare5','B1654BCE','81867239080',HASHBYTES('SHA2_512', 'yessir8802'), 'Robert', 'Jargo', 'Jare',7000000,'LondonNoCap')
+insert into Pengantar values ('dadangvanboer555','AG7567JOR','81365782431',HASHBYTES('SHA2_512', 'howtomaxwin1'), 'Dadang', 'Van', 'Boer',7000000,'LondonNoCap')
+insert into Pengantar values ('yayatdenthol19','AG6324KIR','81290327766',HASHBYTES('SHA2_512', 'darderdor9'), 'Yayat', 'Deden', 'Thol',7000000,'LondonNoCap')
+insert into Pengantar values ('sharford48','B5554BFA','852787363',HASHBYTES('SHA2_512', 'sharford48477'), 'Leelah', 'Samara', 'Harford',7000000,'LondonNoCap')
+
+insert into Pengantar values ('gpatron4c','B7453BCE','832835572',HASHBYTES('SHA2_512', 'gpatron4c356'), 'Fanny', 'Gabe', 'Patron',3000000,'MalangPhd')
+insert into Pengantar values ('cadamovsky4e','B8634FFE','8157703234',HASHBYTES('SHA2_512', 'cadamovsky4e122'), 'Inglis', 'null', 'Adamovsky',3000000,'MalangPhd')
+
+
+select * from Menu
+insert into Menu values ('Regular Meat Lovers', 90000)
+insert into Menu values ('Regular Cheese Lovers', 90000)
+insert into Menu values ('Regular Meat Max Pizza', 112727)
+insert into Menu values ('Jumbo American Favourite', 138182)
+insert into Menu values ('Jumbo Splitza Signature', 147273)
+
+/*
+select * from Pembayaran
+insert into Pembayaran values ('Cash',500000)
+*/
+
+
+
